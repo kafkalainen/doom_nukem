ab1d1cf0f79b88192171eea9a3e70c3983db6b49
kafkalainen <joonas.hj.nivala@gmail.com>
2021-01-19 14:41:10 +0200

    Added draft of the game map.

---
+++ dn_map.h
@@ -0,0 +1,25 @@
+#include "doom_nukem.h"
+
+typedef struct		s_enemy
+{
+	int				hp;
+}					t_enemy;
+
+typedef struct		s_polygon
+{
+	t_xy			x0;
+	t_xy			normal;
+	int				idx_tex;
+	int				connection;
+}					t_polygon;
+
+typedef struct		s_sector
+{
+	t_polygon		*polygons;
+	t_enemy			*enemies;
+	int				nb_of_walls;
+	int				idx_sector;
+	int				tex_floor;
+	int				tex_ceil;
+	int				height;
+}					t_sector;
