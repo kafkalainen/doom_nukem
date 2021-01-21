a34a327d0f47d7c80092424d0fbb7063239ba9cf
kafkalainen <joonas.hj.nivala@gmail.com>
2021-01-21 10:23:12 +0200

    Added.

--- srcs/update_screen/draw_2d.c
+++ srcs/update_screen/draw_2d.c
@@ -6,7 +6,7 @@
 /*   By: jnivala <jnivala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/14 13:27:48 by tmaarela          #+#    #+#             */
-/*   Updated: 2021/01/21 09:53:26 by jnivala          ###   ########.fr       */
+/*   Updated: 2021/01/21 10:20:04 by jnivala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,10 +34,8 @@ void			draw_2d(t_home *home, t_player *plr)
 		i++;
 	}
 	ft_draw_line(temp->x0, home->sectors[0]->polygons->x0, fuchsia, home->draw_surf);
-	plr->pos = vec2(plr->pos.x, plr->pos.y);
-	plr->dir = vec2(plr->dir.x, plr->dir.y);
 	draw_rect_center(plr->pos, vec2(5, 5), home);
-	printf("DIR: %f\n", plr->dir);
-	printf("POS: %f\n", plr->pos);
+	printf("DIR_x: %f\n", plr->dir.x);
+	printf("DIR_y: %f\n", plr->dir.y);
 	ft_draw_line(plr->pos, vec2_add(plr->pos, plr->dir), lightgreen, home->draw_surf);
 }
