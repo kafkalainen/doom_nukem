5f5e429a37e5a901a23f30eafcb13643d83019da
riku zukale <rzukale@student.hive.fi>
2021-02-26 11:10:46 +0200

    Added ft_strsplit and ft_strncpy to libft. Made 2nd pass at creating t_texture from map data file, see png.c for further details

--- srcs/porting/png.c
+++ srcs/porting/png.c
@@ -6,7 +6,7 @@
 /*   By: rzukale <rzukale@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/22 13:43:15 by rzukale           #+#    #+#             */
-/*   Updated: 2021/02/25 15:59:24 by rzukale          ###   ########.fr       */
+/*   Updated: 2021/02/26 11:07:06 by rzukale          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,30 +63,75 @@ void	init_textures(t_home *home)
 }
 
 /*
-** 1st pass at loading char* and converting to uint32. Need to see text output before making further changes
-** 1st index = pitch, after that comes texture height separated by ' ' followed by uint32 data
+** 1st pass at loading char* and converting back to t_texture. Need to see text output before making further changes
 */
 
-unsigned int	*load_texture_from_map_data(char *line)
+void		convert_editor_tex(t_texture *tex)
 {
-	int 	i;
-	int 	pitch;
-	int 	h;
-	Uint32	*tex;
+	unsigned int x;
+	unsigned int y;
 
-	pitch = ft_atoi(line);
-	while (*line != ' ')
-		*line++;
-	h = ft_atoi(line);
-	while (*line != ' ')
-		*line++;
-	if (!(tex = (Uint32*)malloc(sizeof(Uint32) * (pitch * h))))
-		error_output("Memory allocation failed when loading texture from map data\n");
-	i = 0;
-	while (line[i])
+	y = -1;
+	while (++y < tex->h)
 	{
-		tex[i] = ft_atoi(line[i]);
+		x = -1;
+		while (++x < tex->w)
+		{
+			tex->pixels[(y * tex->w) + x] = add_pixel(tex->map_pixels, tex->bpp,
+				((y * tex->pitch) + x * tex->bpp));
+		}
+	}
+	convert_to_uint32(tex->pixels, tex);
+}
+
+void		free_array(unsigned char **array)
+{
+	int i;
+
+	i = 0;
+	while (array[i] != NULL)
+	{
+		ft_strdel(&array[i]);
 		i++;
 	}
+	free(array);
+}
+
+/*
+	** Each map data element type is separated by a element tag (eg. #TEX for textures)
+	** element tag line for all elements will include element tag, total number of element components (eg. #TEX 9)
+	** each element component line will include everything that specific component will need to fully initialize,
+	** they will be separated with a break character
+	** texture element example:
+	** [width] [height] [size] [color_type] [color_depth] [format] [bits_per_pixel] [pitch] [unsigned char *pixel data]
+	** tex idx is determined by the order by which elements are saved into the map data file
+	*/
+
+t_texture	*load_texture_from_map_data(char *line)
+{
+	t_texture		*tex;
+	unsigned char	**elems;		
+
+	if (!(tex = (t_texture*)malloc(sizeof(t_texture))))
+		error_output("Memory allocation of t_texture struct failed\n");
+	elems = ft_strsplit(line, ','); // break character should be a unique character that will not appear in any of the actual data
+	tex->w = ft_atoi(elems[0]);
+	tex->h = ft_atoi(elems[1]);
+	tex->size = ft_atoi(elems[2]);
+	tex->color_type = ft_atoi(elems[3]);
+	tex->color_depth = ft_atoi(elems[4]);
+	tex->format = ft_atoi(elems[5]);
+	tex->bpp = ft_atoi(elems[6]);
+	tex->pitch = ft_atoi(elems[7]);
+	if (!(tex->map_pixels = (unsigned char *)malloc(sizeof(unsigned char) * tex->size)))
+		error_output("Memory allocation of editor pixel pointer failed\n");
+	ft_memcpy(tex->map_pixels, elems[8], tex->size);
+	if (!(tex->pixels = (unsigned int *)malloc(sizeof(unsigned int) *
+		(tex->h * tex->pitch))))
+		error_output("Memory allocation of pixel pointer failed\n");
+	convert_editor_tex(tex);
+	free(tex->map_pixels);
+	tex->map_pixels = NULL;
+	free_array(elems);
 	return (tex);
 }
