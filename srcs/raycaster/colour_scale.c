c9f1a3b283342d9979b0a798ff1f8c84946ebff8
kafkalainen <joonas.hj.nivala@gmail.com>
2021-04-27 13:37:42 +0300

    Added this file.

--- srcs/raycaster/colour_scale.c
+++ srcs/raycaster/colour_scale.c
@@ -6,7 +6,7 @@
 /*   By: jnivala <jnivala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 17:03:27 by jnivala           #+#    #+#             */
-/*   Updated: 2021/04/26 15:20:57 by jnivala          ###   ########.fr       */
+/*   Updated: 2021/04/27 13:13:50 by jnivala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@ Uint32	colour_scale(Uint32 hex, float scale)
 {
 	t_argb	argb;
 
+	if (scale > 1)
+		scale = 1;
+	if (scale < 0)
+		scale = 0;
 	argb.alpha = (hex >> 24);
 	argb.red = (hex >> 16);
 	argb.green = (hex >> 8);
