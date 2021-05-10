66e7d20808eeb3510b8cbff04394c5602aa4589a
riku zukale <rzukale@student.hive.fi>
2021-05-10 12:17:08 +0300

    added prototypes for _write() and _mkdir()

--- headers/syscalls_windows.h
+++ headers/syscalls_windows.h
@@ -6,10 +6,12 @@
 /*   By: rzukale <rzukale@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/22 12:45:10 by jnivala           #+#    #+#             */
-/*   Updated: 2021/05/04 15:58:53 by rzukale          ###   ########.fr       */
+/*   Updated: 2021/05/10 12:16:22 by rzukale          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	_close(int fd);
 int _open(const char *pathname, int flags, int pmode);
 int	_read(int const fd, void const *buffer, unsigned const buffer_size);
+int	_mkdir(const char *dirname);
+int _write(int fd, const void *buffer, unsigned int count);
