a25734fc1dba41b445bd5cf327dcbb9eb1b81d16
kafkalainen <joonas.hj.nivala@gmail.com>
2021-06-15 16:29:09 +0300

    Added ceiling and extra top wall consideration.

--- srcs/update_player/check_y_diff.c
+++ srcs/update_player/check_y_diff.c
@@ -6,7 +6,7 @@
 /*   By: jnivala <jnivala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/03 14:01:12 by jnivala           #+#    #+#             */
-/*   Updated: 2021/06/15 16:08:14 by jnivala          ###   ########.fr       */
+/*   Updated: 2021/06/15 16:27:40 by jnivala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,13 @@
 
 int		check_y_diff(t_player *plr, t_xyz *test_pos, t_sector *to)
 {
-	*test_pos = check_y(to, plr, *test_pos);
+	test_pos->y += plr->height;
 	if (check_distance_to_ceiling(to, test_pos))
-	{
-		ft_putstr("It's too low.");
 		return (1);
-	}
+	test_pos->y -= plr->height;
+	*test_pos = check_y(to, plr, *test_pos);
 	if (test_pos->y - plr->pos.y < plr->height)
 		return (0);
 	else
-	{
-		ft_putstr("It's too high.");
 		return (1);
-	}
 }
