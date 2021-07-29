be2aa1ff9ebaeb317bead11355f23fe3c580ee3e
HandsomeTom <tomi.maarela@gmail.com>
2021-07-29 19:55:51 +0300

    Shooting probs with the point_inside_a_triangle_wall, only one wall works atm. Valve pls fix.

--- srcs/update_player/shooting.c
+++ srcs/update_player/shooting.c
@@ -12,44 +12,74 @@
 
 #include "../../headers/doom_nukem.h"
 
-static int evaluate_ret(t_xyz a, t_xyz b)
-{
-	if (a.x == b.x && a.y == b.y && a.z == b.z)
-		return (TRUE);
-	return (FALSE);
-}
+//static int evaluate_ret(t_xyz a, t_xyz b)
+//{
+//	if (a.x == b.x && a.y == b.y && a.z == b.z)
+//		return (TRUE);
+//	return (FALSE);
+//}
 
 static t_xyz		get_bullet_hit_point(t_home *home, t_player *plr)
 {
-	t_xyz			pos;
-	t_xyz			dir;
-	t_xyz			ret;
-	t_plane			plane;
-	t_wall			*wall;
 	unsigned int	i;
+	t_wall			*walls;
+	t_xyz			dir;
+	t_plane			plane;
+	t_xyz			pos;
 
 	pos = plr->pos;
-	dir = plr->look_dir;
-	wall = home->sectors[plr->cur_sector]->walls;
 	i = 0;
-	ret = (t_xyz){0,0,0};
+	dir = vec3_unit_vector(plr->look_dir);
+	walls = home->sectors[plr->cur_sector]->walls;
 	while (i < home->sectors[plr->cur_sector]->nb_of_walls)
 	{
-		plane.point = wall->top.p[0];
-		plane.normal = wall->top.normal;
-		ret = vec3_intersection_with_ray_and_plane(&plane, pos, dir);
-		if (evaluate_ret(ret, pos) == FALSE)
-		{
-			printf("START\n%f\n%f\n%f\n%d--\n", ret.x, ret.y, ret.z, wall->top.idx);
-			return (ret);
-		}
-		else
-			printf("miss\n");
+		if (point_inside_a_triangle_wall(walls->top.p[0],
+				walls->top.p[1], walls->top.p[2], pos));
+			break ;
+		printf("old wall: %p\n", walls);
+		walls = walls->next;
+		printf("new wall: %p\n", walls);
 		i++;
-		wall = wall->next;
 	}
-	free(wall);
-	return ret;
+	plane.point = walls->top.p[0];
+	plane.normal = walls->top.normal;
+	pos = vec3_intersection_with_ray_and_plane(&plane, pos, dir);
+	printf("START\n%f\n%f\n%f\n%d--\n", pos.x, pos.y, pos.z, walls->top.idx);
+	home->entity_pool[0]->pos = pos;
+
+	return (pos);
+//	t_xyz			pos;
+//	t_xyz			dir;
+//	t_xyz			ret;
+//	t_plane			plane;
+//	t_wall			*wall;
+//	unsigned int	i;
+//
+//	pos = plr->pos;
+//	dir = vec3_unit_vector(plr->look_dir);
+//	dir = vec3_add(pos, vec3_mul(dir, 10.0f));
+//	wall = home->sectors[plr->cur_sector]->walls;
+//	i = 0;
+//	ret = (t_xyz){0,0,0};
+//	while (i < home->sectors[plr->cur_sector]->nb_of_walls)
+//	{
+//		plane.point = wall->top.p[0];
+//		plane.normal = wall->top.normal;
+//		ret = vec3_intersection_with_ray_and_plane(&plane, pos, dir);
+//		if (evaluate_ret(ret, pos) == FALSE)
+//		{
+//			printf("START\n%f\n%f\n%f\n%d--\n", ret.x, ret.y, ret.z, wall->top.idx);
+//			printf("LOOK\n%f\n%f\n%f\n%d--\n", plr->look_dir.x, plr->look_dir.y, plr->look_dir.z);
+//			home->entity_pool[0]->pos = ret;
+//			return (ret);
+//		}
+//		else
+//			printf("miss\n");
+//		i++;
+//		wall = wall->next;
+//	}
+//	free(wall);
+//	return ret;
 }
 
 void			shooting_handle(t_home *home, t_player *plr)
