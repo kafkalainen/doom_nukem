92422ca6006d6d4efbd7d0ea5aa19c07194b462a
kafkalainen <joonas.hj.nivala@gmail.com>
2021-09-03 10:53:56 +0300

    Fixed vec3_angle.

--- libft/src/ft_signf.c
+++ libft/src/ft_signf.c
@@ -6,7 +6,7 @@
 /*   By: jnivala <jnivala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 10:36:59 by jnivala           #+#    #+#             */
-/*   Updated: 2021/09/03 10:39:58 by jnivala          ###   ########.fr       */
+/*   Updated: 2021/09/03 10:50:44 by jnivala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ int	ft_signf(float nb)
 {
 	if (nb < 0)
 		return (-1);
-	else if (nb > 0)
-		return (1);
 	else
-		return (0);
+		return (1);
 }
