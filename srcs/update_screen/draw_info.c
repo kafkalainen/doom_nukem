313d4e124a049a67b4322f60d9accf64c0e60fa4
kafkalainen <joonas.hj.nivala@gmail.com>
2021-09-04 12:42:52 +0300

    Normed draw_info.

--- srcs/update_screen/draw_info.c
+++ srcs/update_screen/draw_info.c
@@ -6,7 +6,7 @@
 /*   By: jnivala <jnivala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/04 11:52:05 by jnivala           #+#    #+#             */
-/*   Updated: 2021/09/04 12:28:32 by jnivala          ###   ########.fr       */
+/*   Updated: 2021/09/04 12:40:57 by jnivala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,29 +38,37 @@ static void	compass_direction(Uint32 *buffer, t_xy *dir, t_plx_modifier mod)
 		ft_str_pxl(buffer, center, "::No dir::", mod);
 }
 
+// Debugging utility function.
+// static void	draw_coordinate(Uint32 *buffer, t_xyz pos)
+// {
+// 	char	*pstr[3];
+
+// 	ft_str_pxl(buffer, (t_xy){5.0f, 150.0f},
+// 	"xyz", (t_plx_modifier){get_color(green), 2, 12});
+// 	pstr[0] = ft_ftoa(pos.x, 6);
+// 	pstr[1] = ft_ftoa(pos.y, 6);
+// 	pstr[2] = ft_ftoa(pos.z, 6);
+// 	ft_str_pxl(buffer, (t_xy){5.0f, 170.0f}, pstr[0],
+// 		(t_plx_modifier){get_color(green), 2, 12});
+// 	ft_str_pxl(buffer, (t_xy){5.0f, 190.0f}, pstr[1],
+// 		(t_plx_modifier){get_color(green), 2, 12});
+// 	ft_str_pxl(buffer, (t_xy){5.0f, 210.0f}, pstr[2],
+// 		(t_plx_modifier){get_color(green), 2, 12});
+// 	free(pstr[0]);
+// 	free(pstr[1]);
+// 	free(pstr[2]);
+// }
 void	draw_info(t_frame *frame, t_player *plr, int nb_fps)
 {
 	char			*fps;
-	char			*plr_pos[3];
 	t_plx_modifier	mod;
 
 	mod.colour = get_color(white);
 	mod.size = TEXT_SIZE;
 	mod.len = 100;
 	fps = ft_itoa(nb_fps);
-	plr_pos[0] = ft_ftoa(plr->pos.x, 6);
-	plr_pos[1] = ft_ftoa(plr->pos.y, 6);
-	plr_pos[2] = ft_ftoa(plr->pos.z, 6);
 	ft_str_pxl(frame->buffer, (t_xy){SCREEN_WIDTH * 0.5 - 15, 0}, fps, mod);
 	compass_direction(frame->buffer, &plr->dir, mod);
-	ft_str_pxl(frame->buffer, (t_xy){5.0f, 150.0f},
-		"player_xyz", (t_plx_modifier){get_color(green), 2, 12});
-	ft_str_pxl(frame->buffer, (t_xy){5.0f, 170.0f}, plr_pos[0],
-		(t_plx_modifier){get_color(green), 2, 12});
-	ft_str_pxl(frame->buffer, (t_xy){5.0f, 190.0f}, plr_pos[1],
-		(t_plx_modifier){get_color(green), 2, 12});
-	ft_str_pxl(frame->buffer, (t_xy){5.0f, 210.0f}, plr_pos[2],
-		(t_plx_modifier){get_color(green), 2, 12});
 	ft_str_pxl(frame->buffer, (t_xy){0, 400}, "x to close minimap", mod);
 	ft_str_pxl(frame->buffer, (t_xy){0, 420}, "c to close info", mod);
 	ft_str_pxl(frame->buffer, (t_xy){0, 420}, "z to pause music", mod);
@@ -68,7 +76,4 @@ void	draw_info(t_frame *frame, t_player *plr, int nb_fps)
 		mod);
 	ft_str_pxl(frame->buffer, (t_xy){0, 460}, "free mouse with m", mod);
 	free(fps);
-	free(plr_pos[0]);
-	free(plr_pos[1]);
-	free(plr_pos[2]);
 }
