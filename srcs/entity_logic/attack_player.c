05a32b3ddfb1b0bb7169c0383495422a00a9c75c
kafkalainen <joonas.hj.nivala@gmail.com>
2021-09-06 15:01:14 +0300

    Attack player.

--- srcs/entity_logic/attack_player.c
+++ srcs/entity_logic/attack_player.c
@@ -6,7 +6,7 @@
 /*   By: jnivala <jnivala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 13:48:43 by jnivala           #+#    #+#             */
-/*   Updated: 2021/09/05 22:59:51 by jnivala          ###   ########.fr       */
+/*   Updated: 2021/09/06 15:00:01 by jnivala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,26 +25,30 @@ static t_bool	shoot_player(t_home *home, t_entity *entity, t_player *plr)
 	return (true);
 }
 
+static t_bool	skull_skulker_attack(t_entity *entity, t_player *plr, Uint32 t)
+{
+	if (get_distance_squared(plr->pos, entity->pos) < 2.25f)
+	{
+		entity->sprite_state = attack;
+		if (entity->anim_offset >= 4
+			&& (int)(entity->cooldown - t) < 0)
+		{
+			player_take_damage(plr, 1, t);
+			play_sound(plr->audio.skull_skulker_attack, 30);
+			return (true);
+		}
+		pick_next_frame(entity, t);
+	}
+	return (false);
+}
+
 t_bool	attack_player(t_home *home, t_entity *entity, t_player *plr,
 		Uint32 t)
 {
 	if (!entity->is_aggroed)
 		return (false);
 	if (entity->type == skull_skulker)
-	{
-		if (get_distance_squared(plr->pos, entity->pos) < 2.25f)
-		{
-			entity->sprite_state = attack;
-			if (entity->anim_offset >= 4
-				&& (int)(entity->cooldown - t) < 0)
-			{
-				player_take_damage(plr, 1, t);
-				play_sound(plr->audio.skull_skulker_attack, 30);
-			}
-			pick_next_frame(entity, t);
-			return (false);
-		}
-	}
+		return (skull_skulker_attack(entity, plr, t));
 	if (entity->type == thing)
 	{
 		entity->sprite_state = attack;
