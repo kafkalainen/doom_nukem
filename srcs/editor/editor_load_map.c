c6de3a4b5210ea3c67db7e517c88e81d23332eee
riku zukale <rzukale@student.hive.fi>
2021-09-10 13:12:24 +0300

    rearranged some button sizes, added idx incrementation to entities during map loading to editor

--- srcs/editor/editor_load_map.c
+++ srcs/editor/editor_load_map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   editor_load_map.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jnivala <jnivala@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: rzukale <rzukale@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/02 19:10:51 by rzukale           #+#    #+#             */
-/*   Updated: 2021/09/10 12:48:16 by jnivala          ###   ########.fr       */
+/*   Updated: 2021/09/10 13:10:10 by rzukale          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -425,13 +425,18 @@ t_entity_list	*new_entity(t_entity_list **head)
 	if (!new)
 		error_output("Memory allocation of new entity failed\n");
 	new->next = NULL;
+	new->entity_idx = 0;
 	if (*head == NULL)
 		*head = new;
 	else
 	{
 		temp = *head;
 		while (temp->next != NULL)
+		{
+			new->entity_idx++;
 			temp = temp->next;
+		}
+		new->entity_idx++;
 		temp->next = new;
 		new->next = NULL;
 	}
