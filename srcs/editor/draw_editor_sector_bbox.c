a2e6af36d7fb2595c84cd4d6f76b2bbf6d7dc46a
kafkalainen <joonas.hj.nivala@gmail.com>
2021-09-15 11:08:39 +0300

    Normed.

--- srcs/editor/draw_editor_sector_bbox.c
+++ srcs/editor/draw_editor_sector_bbox.c
@@ -6,7 +6,7 @@
 /*   By: jnivala <jnivala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 09:31:49 by jnivala           #+#    #+#             */
-/*   Updated: 2021/09/14 14:42:10 by jnivala          ###   ########.fr       */
+/*   Updated: 2021/09/15 11:04:11 by jnivala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ void	draw_bbox_sector(t_editor_sector *sector, t_buffer *buffer,
 	t_box	bbox_scaled;
 
 	bbox_scaled.start = world_to_screen(sector->bbox.start, action->scalarf,
-		action->offsetf, buffer);
+			action->offsetf, buffer);
 	bbox_scaled.end = world_to_screen(sector->bbox.end, action->scalarf,
-		action->offsetf, buffer);
+			action->offsetf, buffer);
 	if (action->selected_sector == sector->idx_sector)
 		color = get_color(wheat);
 	else
--- srcs/editor/draw_ui.c
+++ srcs/editor/draw_ui.c
@@ -6,11 +6,20 @@
 /*   By: jnivala <jnivala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 16:58:32 by jnivala           #+#    #+#             */
-/*   Updated: 2021/09/14 16:58:44 by jnivala          ###   ########.fr       */
+/*   Updated: 2021/09/15 11:08:20 by jnivala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/doom_nukem.h"
+
+static t_bool	initialize_int_string_logic(t_action *action)
+{
+	if (action->set_light_intensity
+		|| action->edit_ceiling_height
+		|| action->edit_floor_height)
+		return (true);
+	return (false);
+}
 
 void	draw_ui(t_editor *editor, t_texture **textures)
 {
@@ -24,13 +33,17 @@ void	draw_ui(t_editor *editor, t_texture **textures)
 	if (editor->action.convert_to_portal)
 		draw_convert_to_portal_line(editor);
 	if (editor->action.save_file)
-		draw_input_string(editor->mapname, &editor->buffer, (editor->buffer.width * 0.5), map_saving);
+		draw_input_string(editor->mapname, &editor->buffer,
+			(editor->buffer.width * 0.5), map_saving);
 	if (editor->action.open_file || editor->action.link_maps)
-		update_editor_load_menu(&editor->buffer, &editor->action, editor->map_names);
-	if (editor->action.set_light_intensity || editor->action.edit_ceiling_height || editor->action.edit_floor_height)
-		draw_int_string_input(&editor->buffer, &editor->action, &editor->int_string);
+		update_editor_load_menu(&editor->buffer, &editor->action,
+			editor->map_names);
+	if (initialize_int_string_logic(&editor->action))
+		draw_int_string_input(&editor->buffer, &editor->action,
+			&editor->int_string);
 	if (editor->action.write_sector_story && editor->temp_sector != NULL)
-		draw_input_string(editor->temp_sector->plot_line, &editor->buffer, (editor->buffer.width * 0.5), story_string);
+		draw_input_string(editor->temp_sector->plot_line,
+			&editor->buffer, (editor->buffer.width * 0.5), story_string);
 	draw_help_text(&editor->action, &editor->buffer);
 	show_editor_usage(&editor->buffer);
 	notify_user(&editor->notification, &editor->buffer,
