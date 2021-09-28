be9cd079b0acddb677dd3d7b98ecda0a958bc27e
riku zukale <rzukale@student.hive.fi>
2021-09-28 19:27:17 +0300

    Fixed light source position when sector width is 1, even in long narrow sectors

--- srcs/parsing/calc_light_sources.c
+++ srcs/parsing/calc_light_sources.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   calc_light_sources.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jnivala <jnivala@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: rzukale <rzukale@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 13:39:57 by jnivala           #+#    #+#             */
-/*   Updated: 2021/09/28 10:22:43 by jnivala          ###   ########.fr       */
+/*   Updated: 2021/09/28 19:15:40 by rzukale          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,10 @@ void	calc_light_sources(t_home *home)
 		if (home->sectors[i]->lights.light_src.y >= 1.0f)
 			home->sectors[i]->lights.light_src.y -= 0.5f;
 		if (vec2_eucl_dist(home->sectors[i]->bounding_box.top_left,
+				home->sectors[i]->bounding_box.bottom_right) < 1.42f
+			|| vec2_eucl_dist(home->sectors[i]->bounding_box.top_left,
+				home->sectors[i]->bounding_box.bottom_left) < 1.42f
+			|| vec2_eucl_dist(home->sectors[i]->bounding_box.top_right,
 				home->sectors[i]->bounding_box.bottom_right) < 1.42f)
 		{
 			centroid = calculate_centroid(home->sectors[i]);
