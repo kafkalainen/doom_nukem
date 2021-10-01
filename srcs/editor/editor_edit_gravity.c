3aef3ac493425569fae0da6abdd8dfdd2460ffa1
riku zukale <rzukale@student.hive.fi>
2021-10-01 12:50:33 +0300

    Fixed a minor mistake in cycling gravity levels

--- srcs/editor/editor_edit_gravity.c
+++ srcs/editor/editor_edit_gravity.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   editor_edit_gravity.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jnivala <jnivala@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: rzukale <rzukale@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 12:01:36 by jnivala           #+#    #+#             */
-/*   Updated: 2021/10/01 12:25:57 by jnivala          ###   ########.fr       */
+/*   Updated: 2021/10/01 12:49:29 by rzukale          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,6 @@ void	editor_edit_gravity(t_editor *editor)
 	else if (editor->temp_sector->gravity == 6)
 		editor->temp_sector->gravity = 10;
 	else if (editor->temp_sector->gravity == 10)
-		editor->temp_sector->gravity = 10;
+		editor->temp_sector->gravity = 2;
 	editor->action.set_gravity = false;
 }
