1ed5af2174919dea6fa7ad26d763dccb66b79368
kafkalainen <joonas.hj.nivala@gmail.com>
2021-10-03 19:18:00 +0300

    Added menu title.

--- srcs/menu_systems/menu_inputs.c
+++ srcs/menu_systems/menu_inputs.c
@@ -6,11 +6,26 @@
 /*   By: jnivala <jnivala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/11 13:55:49 by rzukale           #+#    #+#             */
-/*   Updated: 2021/10/01 16:45:41 by jnivala          ###   ########.fr       */
+/*   Updated: 2021/10/03 19:07:09 by jnivala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/doom_nukem.h"
+
+static void	write_title(t_buffer *buffer, t_plx_modifier *mod)
+{
+	t_xy	coord;
+
+	mod->colour = get_color(yellow);
+	mod->size = MAIN_TITLE_LARGE;
+	ft_c_pxl(buffer, vec2(222, SCREEN_HEIGHT * 0.25f - 9), 'd', *mod);
+	ft_c_pxl(buffer, vec2(550, SCREEN_HEIGHT * 0.25f - 9), 'm', *mod);
+	coord = vec2(0, SCREEN_HEIGHT * 0.25f);
+	mod->len = 8;
+	mod->size = MAIN_TITLE_SMALL;
+	coord.x = center_text_x_axis(0, SCREEN_WIDTH, mod->size, mod->len);
+	ft_str_pxl(buffer, coord, "oom nuke", *mod);
+}
 
 void	update_main_menu(t_buffer *buffer, int *option)
 {
@@ -21,6 +36,7 @@ void	update_main_menu(t_buffer *buffer, int *option)
 	const int			tab[] = {6, 8, 8, 4};
 
 	i = 0;
+	write_title(buffer, &mod);
 	coord = vec2(0, SCREEN_HEIGHT * 0.5f - 45);
 	mod.size = MAIN_MENU_TEXT;
 	while (i < 4)
