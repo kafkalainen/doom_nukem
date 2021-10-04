789c71fe90922426fd7531c9b0dcb9e68022b82e
kafkalainen <joonas.hj.nivala@gmail.com>
2021-10-04 14:43:14 +0300

    fixed problem of checking that points are on same line

--- srcs/editor/check_if_non_convex.c
+++ srcs/editor/check_if_non_convex.c
@@ -6,7 +6,7 @@
 /*   By: jnivala <jnivala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/27 09:05:02 by jnivala           #+#    #+#             */
-/*   Updated: 2021/09/09 12:33:11 by jnivala          ###   ########.fr       */
+/*   Updated: 2021/10/04 14:41:27 by jnivala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static t_xy	vec2_screen_dec(t_screen_xy a, t_screen_xy b)
 	return (vec2_dec(vec2(a.x, a.y), vec2(b.x, b.y)));
 }
 
-int	check_if_non_convex(t_editor_sector *sector)
+t_bool	check_if_non_convex(t_editor_sector *sector)
 {
 	int				i;
 	t_xy			dxy_1;
@@ -27,18 +27,22 @@ int	check_if_non_convex(t_editor_sector *sector)
 
 	i = 0;
 	temp = sector->walls;
-	if (sector->nb_of_walls == 3)
-		return (0);
+	if (sector->nb_of_walls < 3)
+		return (true);
 	while (i < sector->nb_of_walls)
 	{
 		dxy_1 = vec2_screen_dec(temp->next->x0, temp->x0);
 		dxy_2 = vec2_screen_dec(temp->next->next->x0, temp->next->x0);
 		if (i != 0 && is_negative(vec2_cross(dxy_1, dxy_2)) != sign)
-			return (1);
+			return (true);
+		else if (sector->nb_of_walls == 3 && vec2_dot(
+			vec2_screen_dec(temp->next->x0, temp->next->next->x0),
+			vec2_screen_dec(temp->x0, temp->next->x0)) == 1)
+			return (true);
 		else
 			sign = is_negative(vec2_cross(dxy_1, dxy_2));
 		temp = temp->next;
 		i++;
 	}
-	return (0);
+	return (false);
 }
