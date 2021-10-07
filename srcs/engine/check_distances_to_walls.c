fb0730fcd9f7b5e15b545b9f7c2da3264e466a90
kafkalainen <joonas.hj.nivala@gmail.com>
2021-10-07 20:18:37 +0300

    added checks to distance checks.

--- srcs/engine/check_distances_to_walls.c
+++ srcs/engine/check_distances_to_walls.c
@@ -6,7 +6,7 @@
 /*   By: jnivala <jnivala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/13 11:37:18 by jnivala           #+#    #+#             */
-/*   Updated: 2021/10/07 15:45:38 by jnivala          ###   ########.fr       */
+/*   Updated: 2021/10/07 20:18:11 by jnivala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,25 @@ static t_bool	check_if_too_close_to_a_wall(t_wall *wall, float width,
 	t_xyz pos, t_xyz dir)
 {
 	t_xyz	isection;
+	float	pow_width;
 
-	width = width * width;
+	isection = vec3(0.0f, 0.0f, 0.0f);
+	pow_width = width * width;
 	if (wall->top.type < 0)
 	{
-		if (vec3_ray_triangle_intersect(&wall->top, pos, dir, &isection))
-		{
-			if (get_distance_squared(isection, pos) < width)
-				return (true);
-		}
-		if (vec3_ray_triangle_intersect(&wall->bot, pos, dir, &isection))
-		{
-			if (get_distance_squared(isection, pos) < width)
-				return (true);
-		}
-		if (vec2_get_distance_squared(wall->point, vec2(pos.x, pos.z)) < width
-			|| vec2_get_distance_squared(wall->next->point, vec2(pos.x, pos.z))
-			< width)
+		if (vec3_ray_triangle_intersect(&wall->top, pos, dir, &isection)
+			&& get_distance_squared(isection, pos) < pow_width)
+			return (true);
+		if (vec3_ray_triangle_intersect(&wall->bot, pos, dir, &isection)
+			&& get_distance_squared(isection, pos) < pow_width)
+			return (true);
+		if (vec2_get_distance_squared(wall->point, vec2(pos.x, pos.z))
+			< pow_width && wall->bot.p[0].y < (pos.y + width)
+			&& wall->top.p[1].y > (pos.y - width))
+			return (true);
+		if (vec2_get_distance_squared(wall->next->point, vec2(pos.x, pos.z))
+			< pow_width && wall->bot.p[2].y < (pos.y + width)
+			&& wall->top.p[2].y > (pos.y - width))
 			return (true);
 	}
 	return (false);
