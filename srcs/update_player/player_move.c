6926386761afa9962d517432ca08f501aea1e714
kafkalainen <joonas.hj.nivala@gmail.com>
2021-10-11 14:56:49 +0300

    Removed printf call from player move.

--- srcs/update_player/player_move.c
+++ srcs/update_player/player_move.c
@@ -6,7 +6,7 @@
 /*   By: jnivala <jnivala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 16:24:26 by jnivala           #+#    #+#             */
-/*   Updated: 2021/10/11 14:55:32 by jnivala          ###   ########.fr       */
+/*   Updated: 2021/10/11 14:56:23 by jnivala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,6 @@ void	player_place_feet_to_ground(t_player *plr, t_home *home)
 	if (idx == -1)
 	{
 		ft_putendl("Risk of moving out of bounds, move to centroid.");
-		printf("x: %f, y: %f, z: %f\n", plr->pos.x, plr->pos.y, plr->pos.z);
 		plr->pos = vec3(home->sectors[plr->cur_sector]->centroid.x, 100.0f,
 				home->sectors[plr->cur_sector]->centroid.y);
 		find_current_sector(home, plr->pos, plr->cur_sector, &isection);
